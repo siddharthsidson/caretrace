f3246491e0232d2bab4ca08ca7a20584143cc830
siddharthsidson <siddharthsidson@gmail.com>
2025-01-12 18:28:43 +0530

    Latest Changes

--- caretrace/src/app/pages/dashboard-home/dashboard-home.component.ts
+++ caretrace/src/app/pages/dashboard-home/dashboard-home.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { DataService } from '../../service/data.service';
+import { CommonModule } from '@angular/common';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-dashboard-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashboard-home.component.html',
   styleUrl: './dashboard-home.component.css',
 })
-export class DashboardHomeComponent {}
+export class DashboardHomeComponent {
+  helperMaterials: any;
+  tableHeaders: any;
+  materialsArray: any;
+  //videoURL: string = '';
+  videoURL!: SafeResourceUrl; // Safe URL for the iframe
+  @ViewChild('videoIframe') videoIframe!: ElementRef;
+  constructor(
+    private dataService: DataService,
+    private sanitizer: DomSanitizer
+  ) {}
+  ngOnInit(): void {
+    this.GetHelperMaterials();
+  }
+  public GetHelperMaterials() {
+    this.dataService.getHelperMaterials().subscribe({
+      next: (v: any) => {
+        this.helperMaterials = v;
+        console.log(this.helperMaterials);
+        this.tableHeaders = Object.keys(this.helperMaterials[0]);
+        console.log(this.tableHeaders);
+      },
+      error: (e) => console.error(e),
+    });
+  }
+  public HandleMaterials(material: any) {
+    this.materialsArray = material;
+  }
+  public OpenVideo(link: string) {
+    //this.videoURL = link;
+    this.videoURL = this.sanitizer.bypassSecurityTrustResourceUrl(link);
+  }
+  public OpenDocument(link: string){
+    window.open(link, '_blank');
+  }
+  public StopPlayer() {
+    // Access iframe element
+
+    const iframe = this.videoIframe.nativeElement;
+    iframe.src = iframe.src;
+  }
+}
--- caretrace/src/app/service/data.service.ts
+++ caretrace/src/app/service/data.service.ts
@@ -1,11 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
-  constructor() {}
+  //private baseUrl = 'http://192.168.0.113/'; 
+  private baseUrl = 'http://localhost:3000/'; 
+  constructor(private http: HttpClient) {}
   public loggedInUser() {
     return !!localStorage.getItem('careTraceLoginData');
+  }
+  public getHelperMaterials() {
+    let url = this.baseUrl + 'getHelperMaterials';
+    return this.http.get(url);
   }
 }
